2400d77d84a461004f20af3358406b0034a46bf1
Shakil-Ahmmed <Shakilahmmed8882@gmail.com>
2024-03-02 16:49:08 +0600

    chore(remove validator):Eliminate validator from the event shchema

--- src/models/Event.js
+++ src/models/Event.js
@@ -1,4 +1,3 @@
-
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
@@ -11,24 +10,68 @@ const eventSchema = new Schema({
   duration: { type: Number, required: true },
   buffer_time: { type: Number, required: true },
   location: { type: String, required: true },
+  platform: { type: String, required: true },
   participants: [
     {
       name: { type: String, required: true },
       email: { type: String, required: true },
+      image: { type: String, required: true },
+      message: { type: String, required: true },
     },
   ],
-  scheduled_time: { type: Date, required: true },
+  scheduled_time: { type: String, required: false },
   status: { type: String, required: true },
   user: {
     type: String,
-    require:true,
+    require: true,
     ref: "User",
   },
+  availability: [
+    {
+      day: {
+        type: String,
+        required: true,
+        enum: [
+          "Saturday",
+          "Sunday",
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+        ],
+      },
+      slots: [
+        {
+          start_time: {
+            type: String,
+            required: true,
+            // validate: {
+            //   validator: (value) => {
+            //     const timeRegex = /^([01][0-9]|2[0-3]):([0-5][0-9])$/;
+            //     return timeRegex.test(value);
+            //   },
+            //   message: (props) =>
+            //     `${props.value} is not a valid time format (HH:MM)`,
+            // },
+          },
+          end_time: {
+            type: String,
+            required: true,
+            // validate: {
+            //   validator: (value) => {
+            //     const timeRegex = /^([01][0-9]|2[0-3]):([0-5][0-9])$/;
+            //     return timeRegex.test(value);
+            //   },
+            //   message: (props) =>
+            //     `${props.value} is not a valid time format (HH:MM)`,
+            // },
+          },
+        },
+      ],
+    },
+  ],
 });
-
-
-
-
 
 const Event = model("Event", eventSchema);
 
