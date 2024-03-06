d7c29eab6ae122878a735f1392def852eb0a90b4
sadmanryanriad <sadmanryanriad@gmail.com>
2024-03-06 20:34:06 +0600

    send bkash url as response

--- src/routes/bkashRoutes.js
+++ src/routes/bkashRoutes.js
@@ -2,7 +2,7 @@ const { default: axios } = require("axios");
 const express = require("express");
 const router = express.Router();
 const globals = require("node-global-storage");
-const { v4 : uuidv4} = require("uuid");
+const { v4: uuidv4 } = require("uuid");
 
 const bkash_header = async () => {
   return {
@@ -16,19 +16,23 @@ const bkash_header = async () => {
 router.post("/payment/create", async (req, res) => {
   try {
     const { amount } = req.body;
-    const { data } = await axios.post(process.env.bkash_create_payment_url, {
-        mode: '0011',
+    const { data } = await axios.post(
+      process.env.bkash_create_payment_url,
+      {
+        mode: "0011",
         payerReference: " ",
         callbackURL: process.env.local_server_url,
         amount: amount,
         currency: "BDT",
-        intent: 'sale',
-        merchantInvoiceNumber: 'Inv' + uuidv4().substring(0,5)
-    }, {
-        headers: await bkash_header()
-    });
-    console.log(data);
-    // return res.status(200).json({bkashURL:data})
+        intent: "sale",
+        merchantInvoiceNumber: "Inv" + uuidv4().substring(0, 5),
+      },
+      {
+        headers: await bkash_header(),
+      }
+    );
+    // console.log(data);
+    return res.status(200).json({ bkashURL: data.bkashURL });
   } catch (error) {
     console.log(error);
     res.status(500).json({
